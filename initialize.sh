44fdb8f205c285faf942e101b99aed34d3cd5623
Juha <kukiyomenaisapp@gmail.com>
2022-12-31 21:23:18 +0900

    Update initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,53 @@
+echo "Juha Mac Initializer"
+
+# brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# neofetch
+brew install neofetch
+
+# sl
+brew install sl
+
+# cloc
+brew install cloc
+
+# command line tools
+xcode-select --install
+
+# git
+brew install git
+
+# finch
+brew install finch
+
+# vscode
+brew install visual-studio-code --cask
+
+# oh my zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# python
+brew install python
+
+# dotnet
+brew install --cask dotnet
+
+# zig
+brew install zig
+
+
+# clear
+clear
+
+# SL表示
+sl
+
+# 端末情報表示
+neofetch
+
+# brewのリスト表示
+brew list
+
+# 再起動
+zsh
